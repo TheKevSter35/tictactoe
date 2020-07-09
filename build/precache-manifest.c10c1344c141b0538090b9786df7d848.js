af96e0440a2dc6d497417d8615717e4ad72bd005
TheKevSter-NL <43183768+TheKevSter-NL@users.noreply.github.com>
2020-07-09 17:06:23 +0200

    first commit

---
+++ build/precache-manifest.c10c1344c141b0538090b9786df7d848.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac249b1c24023d57b5f9f601155dfdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6578e3a6bf7a8589004",
+    "url": "/static/css/main.4174e10c.chunk.css"
+  },
+  {
+    "revision": "e55ed680437e28c06a1d",
+    "url": "/static/js/2.66198967.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66198967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6578e3a6bf7a8589004",
+    "url": "/static/js/main.8cbc1f45.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
