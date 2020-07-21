1212c0c3e5a04f1bd4fb5dbc47a4c8f6bba927e8
TheKevSter35 <k.curi@hotmail.com>
2020-07-21 14:25:21 +0200

    add draw message, update css

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,42 @@
 import React from 'react';
 
+function calculateWinner(squares) {
+    const lines = [
+        [
+            0, 1, 2
+        ],
+        [
+            3, 4, 5
+        ],
+        [
+            6, 7, 8
+        ],
+        [
+            0, 3, 6
+        ],
+        [
+            1, 4, 7
+        ],
+        [
+            2, 5, 8
+        ],
+        [
+            0, 4, 8
+        ],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a,
+            b,
+            c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+
 class Square extends React.Component {
 
     render() {
@@ -99,7 +136,7 @@ class Game extends React.Component {
         const winner = calculateWinner(current.squares);
         const moves = history.map((step, move) => {
             const desc = move
-                ? 'Go to move #' + move
+                ? 'Go to move #' + move 
                 : 'Go to game start';
             return (
                 <li key={move}>
@@ -109,8 +146,12 @@ class Game extends React.Component {
         });
         let status;
         if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
+            status = 'Winner: ' + winner ;
+        }
+        else if (history.length === 10) {
+            status = 'Draw: '
+        }
+         else {
             status = 'Next player: ' + (this.state.xIsNext
                 ? 'X'
                 : 'O');
@@ -128,39 +169,5 @@ class Game extends React.Component {
         );
     }
 }
-function calculateWinner(squares) {
-    const lines = [
-        [
-            0, 1, 2
-        ],
-        [
-            3, 4, 5
-        ],
-        [
-            6, 7, 8
-        ],
-        [
-            0, 3, 6
-        ],
-        [
-            1, 4, 7
-        ],
-        [
-            2, 5, 8
-        ],
-        [
-            0, 4, 8
-        ],
-        [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a,
-            b,
-            c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    return null;
-}
+
 export default Game;
